c9a9746d035810ebdf1bfe61afe36bbdb56450a6
rafiqbooj <rafiq@booj.com>
2018-08-20 16:14:18 -0600

    Update testFile.php

--- testFile.php
+++ testFile.php
@@ -1,20 +1,25 @@
 <?php
 // this file is to test applying a git patch from github to gitlab using their respective APIs
 
-use Rafiq\Fake\Namespace;
+use Rafiq\Faked\Namespace;
 use Another\Fake\Namespace;
 
-class TestFile extends Nothing
+class TestFile
 {
-    protected $properties = ['Rafiq', 'Booj'];
+    protected $properties = ['Rafiq', 'Booj', 'Remax'];
 
     function __construct()
     {
-        echo "Hello World";
+        echo "Hello World!";
     }
     
     function foo(Bar $bar)
     {
         var_dump($bar);
+    }
+    
+    function another()
+    {
+        // just seeing if it picks up another function
     }
 }
